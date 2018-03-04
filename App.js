7cfecdeaf5b080a977fdc165c286ee7badddfa77
hauce93 <hau-hce@hotmail.com>
2018-03-04 18:59:45 +0800

    Finsh RN Tutorial 1

--- App.js
+++ App.js
@@ -10,7 +10,16 @@ import {
   StyleSheet,
   Text,
   Image,
-  View
+  View,
+  AppRegistry,
+  TextInput,
+  Button,
+  Alert, 
+  TouchableHighlight, TouchableOpacity, TouchableNativeFeedback, TouchableWithoutFeedback,
+  ScrollView,
+  FlatList,
+  SectionList,
+  ActivityIndicator,
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -22,59 +31,447 @@ const instructions = Platform.select({
 
 
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+// type Props = {};
+// export default class App extends Component<Props> {
+//   render() {
 
-    let pic = {
-      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
-    };
+//     let pic = {
+//       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+//     };
     
-    return (
-      //default
-      /*<View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native! CRM 1
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-        <Text style={styles.instructions}>
-          {"\n"} {"\n"} {instructions} 
-        </Text>
+//     return (
+//       //default
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//           Welcome to React Native! CRM 1
+//         </Text>
+//         <Text style={styles.instructions}>
+//           To get started, edit App.js
+//         </Text>
+//         <Text style={styles.instructions}>
+//           {instructions}
+//         </Text>
+//         <Text style={styles.instructions}>
+//           {"\n"} {"\n"} {instructions} 
+//         </Text>
+//       </View>
+      
+
+//       //Image
+//       <Image source={pic} style={{width: 300, height: 110}}/>
+
+//     );
+//   }
+// }
+
+
+// class Greeting extends Component {
+//   render() {
+//     return (
+//       <Text>Hello {this.props.name}!</Text>
+//     );
+//   }
+// }
+
+// export default class LotsOfGreetings extends Component {
+//   render() {
+//     return (
+//       <View style={{alignItems: 'baseline'}}>
+//         <Greeting name='Rexxar' />
+//         <Greeting name='Jaina' />
+//         <Greeting name='Valeera' />
+//       </View>
+//     );
+//   }
+// }
+
+
+
+
+
+// class Blink extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {isShowingText: true};
+
+//     // Toggle the state every second
+//     setInterval(() => {
+//       this.setState(previousState => {
+//         return { isShowingText: !previousState.isShowingText };
+//       });
+//     }, 1000);
+//   }
+
+//   render() {
+//     let display = this.state.isShowingText ? this.props.text : ' ';
+//     return (
+//       <Text>{display}</Text>
+//     );
+//   }
+// }
+
+// export default class BlinkApp extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Blink text='I love to blink' />
+//         <Blink text='Yes blinking is so great' />
+//         <Blink text='Why did they ever take this out of HTML' />
+//         <Blink text='Look at me look at me look at me' />
+//       </View>
+//     );
+//   }
+// }
+
+
+// export default class LotsOfStyles extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text style={styles.red}>just red</Text>
+//         <Text style={styles.bigblue}>just bigblue</Text>
+//         <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+//         <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+//       </View>
+//     );
+//   }
+// }
+
+
+// export default class FixedDimensionsBasics extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
+
+// export default class FlexDimensionsBasics extends Component {
+//   render() {
+//     return (
+//       // Try removing the `flex: 1` on the parent View.
+//       // The parent will not have dimensions, so the children can't expand.
+//       // What if you add `height: 300` instead of `flex: 1`?
+//       <View style={{flex: 1}}>
+//         <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+//         <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+//         <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// }
+
+
+// export default class FlexDirectionBasics extends Component {
+//   render() {
+
+//     var arrView = [];
+//     for(let i = 0; i < 1; i++){
+//       arrView.push(
+//             <View key = {i} style={{flex: 1, flexDirection: 'row'}}>
+//               <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//               <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+//               <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//             </View>
+//           )
+//     }
+
+//     return (
+//       // Try setting `flexDirection` to `column`.
+//       <View style={{flex: 1, flexDirection: 'row'}}>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//         { arrView }
+//       </View>
+//     );
+//   }
+// };
+
+
+// export default class JustifyContentBasics extends Component {
+//   render() {
+//     return (
+//       // Try setting `justifyContent` to `center`.
+//       // Try setting `flexDirection` to `row`.
+//       <View style={{
+//         flex: 1,
+//         flexDirection: 'row',
+//         justifyContent: 'space-between',
+//       }}>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// };
+
+
+// export default class AlignItemsBasics extends Component {
+//   render() {
+//     return (
+//       // Try setting `alignItems` to 'flex-start'
+//       // Try setting `justifyContent` to `flex-end`.
+//       // Try setting `flexDirection` to `row`.
+//       <View style={{
+//         flex: 1,
+//         flexDirection: 'column',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//       }}>
+//         <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}} />
+//         <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}} />
+//       </View>
+//     );
+//   }
+// };
+
+
+
+// export default class PizzaTranslator extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {text: ''};
+//   }
+
+//   render() {
+//     return (
+//       <View style={{padding: 10}}>
+//         <TextInput
+//           style={{height: 40}}
+//           placeholder="Type here to translate!"
+//           onChangeText={(text) => this.setState({text})}
+//         />
+//         <Text style={{padding: 10, fontSize: 42}}>
+//           {this.state.text.split('-').map((word) => word && '🍕').join('***')}
+//         </Text>
+//       </View>
+//     );
+//   }
+// }
+
+// export default class testButton extends Component{
+//   render(){
+//     return(
+//       <Button
+//         onPress={() => {
+//         Alert.alert('You tapped the button!');
+//         }}
+//         title="Press Me"
+//       />
+//     );
+//   }
+// }
+
+
+// export default class ButtonBasics extends Component {
+//   _onPressButton() {
+//     Alert.alert('You tapped the button!')
+//   }
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <View style={styles.buttonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="Press Me"
+//           />
+//         </View>
+//         <View style={styles.buttonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="Press Me"
+//             color="#841584"
+//           />
+//         </View>
+//         <View style={styles.alternativeLayoutButtonContainer}>
+//           <Button
+//             onPress={this._onPressButton}
+//             title="This looks great!"
+//           />
+//           <Button
+//             onPress={this._onPressButton}
+//             title="OK!"
+//             color="#841584"
+//           />
+//         </View>
+//       </View>
+//     );
+//   }
+// }
+
+
+
+// export default class Touchables extends Component {
+//   _onPressButton() {
+//     Alert.alert('You tapped the button!')
+//   }
+
+//   _onLongPressButton() {
+//     Alert.alert('You long-pressed the button!')
+//   }
+
+
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableHighlight</Text>
+//           </View>
+//         </TouchableHighlight>
+//         <TouchableOpacity onPress={this._onPressButton}>
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableOpacity</Text>
+//           </View>
+//         </TouchableOpacity>
+//         <TouchableNativeFeedback
+//             onPress={this._onPressButton}
+//             background={Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : ''}>
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableNativeFeedback</Text>
+//           </View>
+//         </TouchableNativeFeedback>
+//         <TouchableWithoutFeedback
+//             onPress={this._onPressButton}
+//             >
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
+//           </View>
+//         </TouchableWithoutFeedback>
+//         <TouchableHighlight onPress={this._onPressButton} onLongPress={this._onLongPressButton} underlayColor="white">
+//           <View style={styles.button}>
+//             <Text style={styles.buttonText}>Touchable with Long Press</Text>
+//           </View>
+//         </TouchableHighlight>
+//       </View>
+//     );
+//   }
+// }
+
+
+
+
+// export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+//   render() {
+//       return (
+//         <ScrollView>
+//           <Text style={{fontSize:96}}>Scroll me plz</Text>
+//           <Text style={{fontSize:96}}>If you like</Text>
+//           <Text style={{fontSize:96}}>Scrolling down</Text>
+//           <Text style={{fontSize:96}}>What's the best</Text>
+//           <Text style={{fontSize:96}}>Framework around?</Text>
+//           <Image source={require('./react-native/img/favicon.png')} />
+//           <Text style={{fontSize:80}}>React Native</Text>
+//         </ScrollView>
+//     );
+//   }
+// }
+
+
+
+
+// export default class FlatListBasics extends Component {
+//   render() {
+//     return (
+//       <View style={styles.containerList}>
+//         <FlatList
+//           data={[
+//             {key: 'Devin'},
+//             {key: 'Jackson'},
+//             {key: 'James'},
+//             {key: 'Joel'},
+//             {key: 'John'},
+//             {key: 'Jillian'},
+//             {key: 'Jimmy'},
+//             {key: 'Julie'},
+//           ]}
+//           renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+
+
+
+// export default class SectionListBasics extends Component {
+//   render() {
+//     return (
+//       <View style={styles.containerSectionList}>
+//         <SectionList
+//           sections={[
+//             {title: 'D', data: ['Devin']},
+//             {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+//           ]}
+//           renderItem={({item}) => <Text style={styles.itemSectionList}>{item}</Text>}
+//           renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+//           keyExtractor={(item, index) => index}
+//         />
+//       </View>
+//     );
+//   }
+// }
+
+
+
+export default class FetchExample extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
+  }
+
+  componentDidMount(){
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.movies,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    return(
+      <View style={{flex: 1, paddingTop:20}}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
+          keyExtractor={(item, index) => index}
+        />
       </View>
-      */
-
-      //Image
-      <Image source={pic} style={{width: 300, height: 110}}/>
-
     );
   }
 }
 
-
-class Greeting extends Component {
-  render() {
-    return (
-      <Text>Hello {this.props.name}!</Text>
-    );
-  }
-}
-
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'baseline'}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
-      </View>
-    );
-  }
-}
 
 
 
@@ -94,5 +491,50 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
+  },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+  buttonContainer: {
+    margin: 20
+  },
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  //FlatList
+  containerList: {
+   flex: 1,
+   paddingTop: 11
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  //SectionList
+  containerSectionList: {
+   flex: 1,
+   paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  itemSectionList: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
